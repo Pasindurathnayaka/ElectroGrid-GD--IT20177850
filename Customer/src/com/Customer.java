fa6e0e83aba241bfbdff7f96c846cf863831104c
Pasindu Rathnayaka <88796335+Pasindurathnayaka@users.noreply.github.com>
2022-05-12 15:05:23 +0530

    Insert

--- Customer/src/com/Customer.java
+++ Customer/src/com/Customer.java
@@ -24,3 +24,39 @@ public class Customer {
 		}
 		return con;
 	}
+
+	public String insertCustomer(String cusName, String cusAddress, String cusEmail, String cusPhone)  
+	{   
+		String output = ""; 	 
+		try   
+		{    
+			Connection con = connect(); 
+	 
+			if (con == null)    
+			{return "Error while connecting to the database for inserting."; } 
+	 
+			// create a prepared statement 
+			String query = " insert into cus2(`cusID`,`cusName`,`cusAddress`,`cusEmail`,`cusPhone`)" + " values (?, ?, ?, ?, ?)"; 
+			PreparedStatement preparedStmt = con.prepareStatement(query); 
+	 
+			// binding values    
+			 preparedStmt.setInt(1, 0);
+			 preparedStmt.setString(2, cusName);
+			 preparedStmt.setString(3, cusAddress);
+			 preparedStmt.setString(4, cusEmail);
+			 preparedStmt.setString(5, cusPhone);
+			
+			// execute the statement    
+			preparedStmt.execute();    
+			con.close(); 
+	   
+			String newCustomer = readCustomer(); 
+			output =  "{\"status\":\"success\", \"data\": \"" + newCustomer + "\"}";    
+		}   
+		catch (Exception e)   
+		{    
+			output =  "{\"status\":\"error\", \"data\": \"Error while inserting the Customer.\"}";  
+			System.err.println(e.getMessage());   
+		} 		
+	  return output;  
+	} 	
