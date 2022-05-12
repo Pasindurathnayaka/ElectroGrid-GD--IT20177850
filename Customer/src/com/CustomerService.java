168d6ada20046888c3e912a725f7bc0fad275acd
Pasindu Rathnayaka <88796335+Pasindurathnayaka@users.noreply.github.com>
2022-05-12 17:30:58 +0530

    get

--- Customer/src/com/CustomerService.java
+++ Customer/src/com/CustomerService.java
@@ -1,5 +1,34 @@
 package com;
 
-public class CustomerService {
+import com.Customer;
+import java.io.IOException; 
+import java.util.HashMap; 
+import java.util.Map; 
+import java.util.Scanner;
 
-}
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/CustomerService")
+public class CustomerService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	Customer customerObj = new Customer();
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CustomerService() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
