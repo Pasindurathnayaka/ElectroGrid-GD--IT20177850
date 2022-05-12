bfef8df2038115deb3716c4e710e40fd187446f8
Pasindu Rathnayaka <88796335+Pasindurathnayaka@users.noreply.github.com>
2022-05-12 18:13:57 +0530

    delete

--- Customer/src/com/CustomerService.java
+++ Customer/src/com/CustomerService.java
@@ -56,3 +56,40 @@ public class CustomerService extends HttpServlet {
 		 
 		 			response.getWriter().write(output);
 	}
+
+	
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		Map paras = getParasMap(request); 
+		 
+		 String output = customerObj.deleteCustomer(paras.get("cusID").toString());  
+		 
+		 response.getWriter().write(output);
+	}
+	
+	// Convert request parameters to a Map
+		private static Map getParasMap(HttpServletRequest request)
+		{
+		 Map<String, String> map = new HashMap<String, String>();
+		try
+		 { 
+		 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+		 String queryString = scanner.hasNext() ?
+		 scanner.useDelimiter("\\A").next() : "";
+		 scanner.close();
+		 String[] params = queryString.split("&");
+		 for (String param : params)
+		 { 
+		
+		String[] p = param.split("=");
+		 map.put(p[0], p[1]);
+		 }
+		 }
+		catch (Exception e)
+		 {
+		 }
+		return map;
+		}
+
+}
