1d22ecefb78b2f2d44b17f560b5bd2b0a3d42b03
Pasindu Rathnayaka <88796335+Pasindurathnayaka@users.noreply.github.com>
2022-05-12 11:11:13 +0530

    create database

--- Customer/src/com/Customer.java
+++ Customer/src/com/Customer.java
@@ -1,5 +1,26 @@
 package com;
 
-public class Customer {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-}
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+
+public class Customer {
+	
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/egsystem?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
