a5a866ce32ae0e07b001ee2a18ca85a0e1ad08b0
Pasindu Rathnayaka <88796335+Pasindurathnayaka@users.noreply.github.com>
2022-05-12 21:22:32 +0530

    validations

---
+++ Customer/WebContent/Components/valicustomer.js
@@ -0,0 +1,167 @@
+$(document).ready(function() 
+{  
+		$("#alertSuccess").hide();  
+	    $("#alertError").hide(); 
+}); 
+ 
+ 
+// SAVE ============================================ 
+$(document).on("click", "#btnSave", function(event) 
+{  
+	// Clear alerts---------------------  
+	$("#alertSuccess").text("");  
+	$("#alertSuccess").hide();  
+	$("#alertError").text("");  
+	$("#alertError").hide(); 
+ 
+	// Form validation-------------------  
+	var status = validateCustomerForm();  
+	if (status != true)  
+	{   
+		$("#alertError").text(status);   
+		$("#alertError").show();   
+		return;  
+	} 
+ 
+	// If valid------------------------  
+	var type = ($("#hidCustomerIDSave").val() == "") ? "POST" : "PUT"; 
+
+	$.ajax( 
+	{  
+			url : "CustomerService",  
+			type : type,  
+			data : $("#formCustomer").serialize(),  
+			dataType : "text",  
+			complete : function(response, status)  
+			{   
+				onCustomerSaveComplete(response.responseText, status);  
+			} 
+	}); 
+}); 
+
+
+function onCustomerSaveComplete(response, status) 
+{  
+	if (status == "success")  
+	{   
+		var resultSet = JSON.parse(response); 
+
+		if (resultSet.status.trim() == "success")   
+		{    
+			$("#alertSuccess").text("Successfully saved.");    
+			$("#alertSuccess").show(); 
+
+			$("#divCustomerGrid").html(resultSet.data);   
+		} else if (resultSet.status.trim() == "error")   
+		{    
+			$("#alertError").text(resultSet.data);    
+			$("#alertError").show();   
+		} 
+
+	} else if (status == "error")  
+	{   
+		$("#alertError").text("Error while saving.");   
+		$("#alertError").show();  
+	} else  
+	{   
+		$("#alertError").text("Unknown error while saving..");   
+		$("#alertError").show();  
+	} 
+
+	$("#hidCustomerIDSave").val("");  
+	$("#formCustomer")[0].reset(); 
+} 
+
+ 
+// UPDATE========================================== 
+$(document).on("click", ".btnUpdate", function(event) 
+{     
+	$("#hidCustomerIDSave").val($(this).closest("tr").find('#hidCustomerIDUpdate').val());     
+	$("#cusName").val($(this).closest("tr").find('td:eq(0)').text());     
+	$("#cusAddress").val($(this).closest("tr").find('td:eq(1)').text());
+	$("#cusEmail").val($(this).closest("tr").find('td:eq(2)').text());
+	$("#cusPhone").val($(this).closest("tr").find('td:eq(3)').text());     
+}); 
+
+
+
+
+//REMOVE===========================================
+$(document).on("click", ".btnRemove", function(event) 
+{  
+	$.ajax(  
+	{   
+		url : "CustomerService",   
+		type : "DELETE",   
+		data : "cusID=" + $(this).data("customerid"),   
+		dataType : "text",   
+		complete : function(response, status)   
+		{    
+			onCustomerDeleteComplete(response.responseText, status);   
+		}  
+	}); 
+}); 
+
+function onCustomerDeleteComplete(response, status) 
+{  
+	if (status == "success")  
+	{   
+		var resultSet = JSON.parse(response); 
+
+		if (resultSet.status.trim() == "success")   
+		{    
+			
+			$("#alertSuccess").text("Successfully deleted.");    
+			$("#alertSuccess").show(); 
+		
+			$("#divCustomerGrid").html(resultSet.data); 
+			
+		} else if (resultSet.status.trim() == "error")   
+		{    
+			$("#alertError").text(resultSet.data);    
+			$("#alertError").show();   
+		}
+		
+
+	} else if (status == "error")  
+	{   
+		$("#alertError").text("Error while deleting.");   
+		$("#alertError").show();  
+	} else  
+	{   
+		$("#alertError").text("Unknown error while deleting..");   
+		$("#alertError").show();  
+	}
+}
+ 
+// CLIENT-MODEL========================================================================= 
+function validateCustomerForm() 
+{  
+	// NAME  
+	if ($("#cusName").val().trim() == "")  
+	{   
+		return "Insert Name.";  
+	} 
+
+	// ADDRESS------------------------  
+	if ($("#cusAddress").val().trim() == "")  
+	{   
+		return "Insert Address.";  
+	} 
+	
+	// EMAIL------------------------  
+	if ($("#cusEmail").val().trim() == "")  
+	{   
+		return "Insert Email.";  
+	} 
+	
+	
+	//PHONE-------------------------------
+	 var tmpPhone = $("#cusPhone").val().trim();
+	if (!$.isNumeric(tmpPhone)) 
+	 {
+	 return "Insert Phone No.";
+	 }
+
+	return true; 
+}
